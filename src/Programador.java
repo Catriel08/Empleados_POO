ac565cb41b501f7d8e908e345f9a36faf564ff8d
Brayan <brayangrimaldo2004@gmail.com>
2023-09-29 14:35:23 -0500

    Update

--- src/Programador.java
+++ src/Programador.java
@@ -8,4 +8,34 @@ public class Programador extends Empleado {
         this.numero_lineas_por_hora = numero_lineas_por_hora;
         this.lenguaje_programa = lenguaje_programa;
     }
+
+    public Programador() {
+        super();
+        this.numero_lineas_por_hora = "";
+        this.lenguaje_programa = "";
+    }
+
+    public String getNumero_lineas_por_hora() {
+        return numero_lineas_por_hora;
+    }
+
+    public void setNumero_lineas_por_hora(String numero_lineas_por_hora) {
+        this.numero_lineas_por_hora = numero_lineas_por_hora;
+    }
+
+    public String getLenguaje_programa() {
+        return lenguaje_programa;
+    }
+
+    public void setLenguaje_programa(String lenguaje_programa) {
+        this.lenguaje_programa = lenguaje_programa;
+    }
+
+    @Override
+    public String toString() {
+        return "Programador{" +
+                "numero_lineas_por_hora='" + numero_lineas_por_hora + '\'' +
+                ", lenguaje_programa='" + lenguaje_programa + '\'' +
+                '}';
+    }
 }
