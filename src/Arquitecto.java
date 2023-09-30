203e41fcc29c0bbffbac79619a6b7171d3c7c812
Brayan <brayangrimaldo2004@gmail.com>
2023-09-29 21:20:14 -0500

    Update

---
+++ src/Arquitecto.java
@@ -0,0 +1,31 @@
+public class Arquitecto extends Empleado {
+
+    private int cantidad_proyectos;
+
+
+    public Arquitecto(String nombre, String identificacion, int edad, String estado_civil, float salario, int cantidad_proyectos) {
+        super(nombre, identificacion, edad, estado_civil, salario);
+        this.cantidad_proyectos = cantidad_proyectos;
+    }
+
+    public Arquitecto() {
+        super();
+        this.cantidad_proyectos = 0;
+    }
+
+
+    public int getCantidad_proyectos() {
+        return cantidad_proyectos;
+    }
+
+    public void setCantidad_proyectos(int cantidad_proyectos) {
+        this.cantidad_proyectos = cantidad_proyectos;
+    }
+
+    @Override
+    public String toString() {
+        return "Arquitecto{" +
+                "cantidad_proyectos=" + cantidad_proyectos +
+                '}';
+    }
+}
---
+++ src/Director.java
@@ -0,0 +1,29 @@
+public class Director extends Empleado {
+
+    private String years_experience;
+
+    public Director(String nombre, String identificacion, int edad, String estado_civil, float salario, String years_experience) {
+        super(nombre, identificacion, edad, estado_civil, salario);
+        this.years_experience = years_experience;
+    }
+
+    public Director() {
+        super();
+        this.years_experience = "";
+    }
+
+    public String getYears_experience() {
+        return years_experience;
+    }
+
+    public void setYears_experience(String years_experience) {
+        this.years_experience = years_experience;
+    }
+
+    @Override
+    public String toString() {
+        return "Director{" +
+                "years_experience='" + years_experience + '\'' +
+                '}';
+    }
+}
